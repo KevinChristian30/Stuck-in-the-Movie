5c646a2c7c87e06c3358398a06d5dd6d0d81e07d
Kevin Christian <kdotchrist30@gmail.com>
2022-12-24 13:43:07 +0700

    Fixed Resignation Letter Status

--- application/src/Components/ResignationLetterCard/ResignationLetterCard.js
+++ application/src/Components/ResignationLetterCard/ResignationLetterCard.js
@@ -5,10 +5,12 @@ const ResignationLetterCard = (props) => {
 
   const url = props.url;
   const count = props.count;
+  const status = props.status;
 
   return ( 
     <div className="resignation-letter-card">
       <p>Resignation Request {count}</p>
+      <p>{status}</p>
       <a id="file-link" target="_blank" href={url}>View</a>
     </div>
    );
--- application/src/View/HumanResource/ResignationLetterView/ResignationLetterView.js
+++ application/src/View/HumanResource/ResignationLetterView/ResignationLetterView.js
@@ -1,25 +1,27 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useLayoutEffect } from "react";
 import NavBar from "../../../Components/NavBar/NavBar";
 import Title from "../../../Components/Title/Title";
 import "./ResignationLetterView.css";
-import { storage } from "../../../Utility/firebase-config";
+import { db, storage } from "../../../Utility/firebase-config";
 import { getDownloadURL, listAll, ref, uploadBytes } from "firebase/storage";
 import { v4 } from 'uuid';
 import ResignationLetterController from "../../../Controller/ResignationLetterController";
 import ResignationLetterCard from "../../../Components/ResignationLetterCard/ResignationLetterCard";
-import DataTable from "react-data-table-component";
+import { collection, getDocs } from "firebase/firestore";
 
 const ResignationLetterView = () => {
 
   const [file, setFile] = useState(null);
   const [resignationLetters, setResignationLetters] = useState([]);
+  const [resignationLettersCollection, setResignationLettersCollection] = useState([]);
 
   const imageListRef = ref(storage, `resignation-letter/${global.CurrentUser.EmployeeEmail}` + '/');
 
   let displayedLetter = {};
+  const [itemsToURL, setItemsToURL] = useState({});
   let c = 1;
 
-  const uploadImage = (e) => {
+  const uploadFile = (e) => {
 
     e.preventDefault()
     if (file == null) return;
@@ -27,7 +29,7 @@ const ResignationLetterView = () => {
     const identifier = v4();
 
     const fileRef = ref(storage, `resignation-letter/${global.CurrentUser.EmployeeEmail + '/' + identifier}`);
-    uploadBytes(fileRef, file).then(() => {
+    uploadBytes(fileRef, file).then((response) => {
 
       ResignationLetterController.createResignationLetter(identifier);
       alert("File Uploaded");
@@ -42,15 +44,44 @@ const ResignationLetterView = () => {
 
       response.items.forEach((item) => {
         
-        getDownloadURL(item).then((url) => {
-          setResignationLetters(prev => [...prev, url]);
+        setResignationLetters(prev => [...prev, item]);
+        
+        getDownloadURL(item).then(url => {
+
+          const newItem = {};
+          newItem[item.name] = url;
+
+          setItemsToURL(itemsToURL => ({
+            ...itemsToURL,
+            ...newItem
+          }))
+
         });
 
       });
 
     });
 
+    const getResignationLettersCollection = async () => {
+
+      const resignationLettersCollectionRef = collection(db, 'resignation-letters');
+      const data = await getDocs(resignationLettersCollectionRef);
+      setResignationLettersCollection(data.docs.map(doc => ({...doc.data(), id: doc.id})));
+
+    }
+
+    getResignationLettersCollection();
+
   }, []); 
+
+  const getStatus = (item) => {
+
+    for (let i = 0; i < resignationLettersCollection.length; i++){
+      if (item.name == resignationLettersCollection[i].ResignationLetterIdentifier) 
+      return resignationLettersCollection[i].ResignationLetterStatus;
+    }
+
+  }
 
   return ( 
 
@@ -62,15 +93,15 @@ const ResignationLetterView = () => {
           <Title title="My Resignation Letters"/>
           <form action="" className="file-input">
             <input type="file" onChange={ e => {setFile(e.target.files[0])} } />
-            <button onClick={ uploadImage } id="submit-button">Submit Resignation Letter</button>
+            <button onClick={ uploadFile } id="submit-button">Submit Resignation Letter</button>
           </form>
       </div>
 
       {
-        resignationLetters.map((url) => {
-          if (!displayedLetter[url]){
-            displayedLetter[url] = true;
-            return <ResignationLetterCard key={url} url={url} identifier={'asd'} count={c++}/>
+        resignationLetters.map((item) => {
+          if (!displayedLetter[item]){
+            displayedLetter[item] = true;
+            return <ResignationLetterCard key={item} status={ getStatus(item) } url={ itemsToURL[item.name] } count={c++}/>
           }
         })
       }
